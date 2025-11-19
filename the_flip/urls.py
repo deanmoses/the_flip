2c3b71dedb38d84e5d595860c83b13d6f2284340
Dean Moses <moses@tacocat.com>
2025-11-18 18:39:35 -0700

    Commenting

--- the_flip/urls.py
+++ the_flip/urls.py
@@ -34,7 +34,11 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 path("", RedirectView.as_view(pattern_name="problem-report-list", permanent=False), name="home"),
+    
+    # Django admin app
     path("admin/", admin.site.urls),
+
+    # Authentication
     path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 
