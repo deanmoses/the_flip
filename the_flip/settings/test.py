51819aea2223f6f75701b1f370d23b0a85ba8952
Dean Moses <moses@tacocat.com>
2025-11-30 10:46:41 -0800

    CI now uses Postgres instead of SQLite

--- the_flip/settings/test.py
+++ the_flip/settings/test.py
@@ -1,8 +1,12 @@
+import dj_database_url
+
 from .base import *  # noqa
 
 DEBUG = False
 PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
-DATABASES["default"] = {
-    "ENGINE": "django.db.backends.sqlite3",
-    "NAME": ":memory:",
-}
+
+# Use DATABASE_URL if provided (CI uses Postgres), otherwise SQLite for local dev
+DATABASES["default"] = dj_database_url.config(  # type: ignore[assignment]
+    default="sqlite://:memory:",
+    conn_max_age=600,
+)
